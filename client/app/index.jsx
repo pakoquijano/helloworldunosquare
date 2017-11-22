e62b44339bc655d16eb1910d9a195da335f1a9fa
Francisco Quijano <pakoquijano@gmail.com>
2017-11-21 18:20:09 -0600

    node modules gitignore

---
+++ client/app/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component {
+  render() {
+    return <div>Hola luis</div>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const BUILD_DIR = path.resolve(__dirname, "client/build");
+const APP_DIR = path.resolve(__dirname, "client/app");
+
+const config = {
+  entry: APP_DIR + "/index.jsx",
+  output: {
+    path: BUILD_DIR,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
+
+module.exports = config;
